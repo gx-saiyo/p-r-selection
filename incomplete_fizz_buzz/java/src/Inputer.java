3edbbf42f2722af70297ea41e5ceb8d970c6d45f
阪田祐宇 <fefe@fefebook.local>
2017-03-17 15:44:55 +0900

    Inputerクラス:非推奨の解消

--- incomplete_fizz_buzz/java/src/Inputer.java
+++ incomplete_fizz_buzz/java/src/Inputer.java
@@ -1,13 +1,21 @@
-import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Inputer implements Inputable {
 
   @Override
   public String gets() {
     try {
-      DataInputStream stream = new DataInputStream(System.in);
-      return stream.readLine();
-    } catch(Exception e) {}
+//        非推奨
+//      DataInputStream stream = new DataInputStream(System.in);
+//      return stream.readLine();
+
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      return reader.readLine();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     return null;
   }
 }
