46fbb4bbeffe6bd1165575a84dfb1438da220dd4
nishinotakuma <takuuma.717@gmail.com>
2017-03-20 16:01:39 +0900

    共通部分統合

--- incomplete_fizz_buzz/ruby/lib/fizz_buzz.rb
+++ incomplete_fizz_buzz/ruby/lib/fizz_buzz.rb
@@ -1,49 +1,37 @@
 module FizzBuzz
+  module_function
 
   def fizz_buzz(number, printer)
-    if number == 0
-      printer.execute(number.to_s)
-    elsif number % 15 == 0
-      printer.execute('FizzBuzz')
-    elsif number % 3 == 0
-      printer.execute('Fizz')
-    elsif number % 5 == 0
-      printer.execute('Buzz')
-    else
-      printer.execute(number.to_s)
-    end
+    str = discriminator(number)
+    printer.execute(str)
   end
 
   def fizz_buzz_history(number, printer)
-    if number == 0
-      printer.execute("#{number}, #{number.to_s}")
-    elsif number % 15 == 0
-      printer.execute("#{number}, FizzBuzz")
-    elsif number % 3 == 0
-      printer.execute("#{number}, Fizz")
-    elsif number % 5 == 0
-      printer.execute("#{number}, Buzz")
-    else
-      printer.execute("#{number}, #{number.to_s}")
-    end
+    str = discriminator(number)
+    printer.execute("#{number}, #{str}")
   end
 
   def write(histories)
     file = File.new('data.txt', 'w')
     for i in 0 .. (histories.size - 1) do
       history = histories[i]
-      if history == 0
-        file.puts("#{history}, #{history.to_s}")
-      elsif history % 15 == 0
-        file.puts("#{history}, FizzBuzz")
-      elsif history % 3 == 0
-        file.puts("#{history}, Fizz")
-      elsif history % 5 == 0
-        file.puts("#{history}, Buzz")
-      else
-        file.puts("#{history}, #{history.to_s}")
-      end
+      str = discriminator(history)
+      file.puts("#{history}, #{str}")
     end
     file.close
+  end
+
+  def discriminator(number)
+    if number == 0
+      number.to_s
+    elsif number % 15 == 0
+      "FizzBuzz"
+    elsif number % 3 == 0
+      "Fizz"
+    elsif number % 5 == 0
+      "Buzz"
+    else
+      number.to_s
+    end
   end
 end
--- incomplete_fizz_buzz/ruby/lib/fizz_buzz_runner.rb
+++ incomplete_fizz_buzz/ruby/lib/fizz_buzz_runner.rb
@@ -1,8 +1,6 @@
 require 'fizz_buzz'
 
 class FizzBuzzRunner
-  include FizzBuzz
-
   def initialize(inputer, printer)
     @inputer = inputer
     @printer = printer
@@ -13,13 +11,13 @@ class FizzBuzzRunner
     if selector == '1'
       number = @inputer.gets.to_i
       @histories << number
-      fizz_buzz(number, @printer)
+      FizzBuzz.fizz_buzz(number, @printer)
     elsif selector == '2'
       for i in 0 .. (@histories.size - 1) do
-        fizz_buzz_history(@histories[i], @printer)
+        FizzBuzz.fizz_buzz_history(@histories[i], @printer)
       end
     elsif selector == '3'
-      write(@histories)
+      FizzBuzz.write(@histories)
     elsif selector == '4'
       if File.exist?('data.txt')
         file = File.new('data.txt', 'r')
