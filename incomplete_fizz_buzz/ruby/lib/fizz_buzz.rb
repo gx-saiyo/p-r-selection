947fc9a9641b489d6c6f7c5c32584f03cecf0b6d
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-04-05 21:47:06 +0900

    fix fizz_buzz method and fizz_buzz_history method and write method to fizz_buzz.rb

--- incomplete_fizz_buzz/ruby/lib/fizz_buzz.rb
+++ incomplete_fizz_buzz/ruby/lib/fizz_buzz.rb
@@ -2,49 +2,39 @@ module FizzBuzz
   module_function
 
   def fizz_buzz(number, printer)
-    if number == 0
-      printer.execute(number.to_s)
-    elsif number % 15 == 0
-      printer.execute('FizzBuzz')
-    elsif number % 3 == 0
-      printer.execute('Fizz')
-    elsif number % 5 == 0
-      printer.execute('Buzz')
-    else
-      printer.execute(number.to_s)
-    end
+    judge = judgment(number)
+    printer.execute(judge)
   end
 
   def fizz_buzz_history(number, printer)
-    if number == 0
-      printer.execute("#{number}, #{number.to_s}")
-    elsif number % 15 == 0
-      printer.execute("#{number}, FizzBuzz")
-    elsif number % 3 == 0
-      printer.execute("#{number}, Fizz")
-    elsif number % 5 == 0
-      printer.execute("#{number}, Buzz")
-    else
-      printer.execute("#{number}, #{number.to_s}")
-    end
+    judge = judgment(number)
+    printer.execute("#{number}, #{judge}")
   end
 
   def write(histories)
     file = File.new('data.txt', 'w')
-    for i in 0 .. (histories.size - 1) do
-      history = histories[i]
-      if history == 0
-        file.puts("#{history}, #{history.to_s}")
-      elsif history % 15 == 0
-        file.puts("#{history}, FizzBuzz")
-      elsif history % 3 == 0
-        file.puts("#{history}, Fizz")
-      elsif history % 5 == 0
-        file.puts("#{history}, Buzz")
-      else
-        file.puts("#{history}, #{history.to_s}")
-      end
+    histories.each do |history|
+      judge = judgment(history)
+      file.puts("#{history}, #{judge}")
     end
     file.close
+  end
+
+  def judgment(number)
+    fizz = number % 3
+    buzz = number % 5
+
+    if number == 0
+      number.to_s
+    elsif (fizz == 0 && buzz == 0)
+      "FizzBuzz"
+    elsif fizz == 0
+      "Fizz"
+    elsif buzz == 0
+      "Buzz"
+    else
+      number.to_s
+    end
+
   end
 end
