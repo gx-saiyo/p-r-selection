0fd015cf337a0c1e1c5e998d81005fc5a5582786
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-04-08 16:51:20 +0900

    remove judge variable from fizz_buzz.rb

--- incomplete_fizz_buzz/ruby/lib/fizz_buzz.rb
+++ incomplete_fizz_buzz/ruby/lib/fizz_buzz.rb
@@ -2,22 +2,19 @@ module FizzBuzz
   module_function
 
   def fizz_buzz(number, printer)
-    judge = judgment(number)
-    printer.execute(judge)
+    printer.execute judgment(number)
   end
 
   def fizz_buzz_history(histories, printer)
     histories.each do |history|
-      judge = judgment(history)
-      printer.execute("#{history}, #{judge}")
+      printer.execute("#{history}, #{ judgment(history) }")
     end
   end
 
   def write(histories)
     file = File.new('data.txt', 'w')
     histories.each do |history|
-      judge = judgment(history)
-      file.puts("#{history}, #{judge}")
+      file.puts("#{history}, #{ judgment(history) }")
     end
     file.close
   end
