647323069a943c97c81f366bbc7161e0ce1bb85b
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-04-08 14:29:32 +0900

    move Fizz to fizz_buzz.rb

--- incomplete_fizz_buzz/ruby/lib/fizz_buzz.rb
+++ incomplete_fizz_buzz/ruby/lib/fizz_buzz.rb
@@ -37,8 +37,8 @@ module FizzBuzz
 
   def judgment(number)
     return "FizzBuzz" if number % 15 == 0
-    return "Fizz"     if number % 3 == 0
     return "Buzz"     if number % 5 == 0
+    return "Fizz"     if number % 3 == 0
 
     number.to_s
   end
