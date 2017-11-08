5412cf4e3df3d43acdd5e458add2f750a3036563
chanyou0311 <yuukun0311@gmail.com>
2017-11-08 14:28:56 +0900

    [clean]インデントを4スペースに変更

--- incomplete_fizz_buzz/python/src/looper.py
+++ incomplete_fizz_buzz/python/src/looper.py
@@ -1,12 +1,12 @@
 class Looper:
 
-  def __init__(self, fizz_buzz_runner, inputer):
-    self.fizz_buzz_runner = fizz_buzz_runner
-    self.inputer = inputer
+    def __init__(self, fizz_buzz_runner, inputer):
+        self.fizz_buzz_runner = fizz_buzz_runner
+        self.inputer = inputer
 
-  def loop(self):
-    while True:
-      selector = self.inputer.gets()
-      if selector == '0':
-        return
-      self.fizz_buzz_runner.run(selector)
+    def loop(self):
+        while True:
+            selector = self.inputer.gets()
+            if selector == '0':
+                return
+            self.fizz_buzz_runner.run(selector)
