9080b3db8752c12ca55382e6526a17c8d3858396
chanyou0311 <yuukun0311@gmail.com>
2017-11-10 11:29:12 +0900

    [add] printerクラスに色指定して出力するメソッドを追加

--- incomplete_fizz_buzz/python/src/printer.py
+++ incomplete_fizz_buzz/python/src/printer.py
@@ -2,3 +2,13 @@ class Printer:
 
     def execute(self, s):
         print('result: %s' % s)
+
+    def error(self, s):
+        print(self.ng + s + self.end)
+
+    def warning(self, s):
+        print(self.warn + s + self.end)
+
+    warn = '\033[93m'
+    ng = '\033[91m'
+    end = '\033[0m'
