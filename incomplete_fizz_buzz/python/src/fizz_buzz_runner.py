1f4ba3718e04fa3a839b69cb5cf561c341319978
chanyou0311 <yuukun0311@gmail.com>
2017-11-10 15:10:51 +0900

    [add] 日時の読書き機能を追加

--- incomplete_fizz_buzz/python/src/fizz_buzz_runner.py
+++ incomplete_fizz_buzz/python/src/fizz_buzz_runner.py
@@ -1,4 +1,6 @@
 import fizz_buzz
+import datetime
+import re
 from glob import glob
 
 class FizzBuzzRunner:
@@ -30,8 +32,28 @@ class FizzBuzzRunner:
             else:
                 self.printer.warning('The history is empty.')
         elif selector == '3':
-            fizz_buzz.write(self.histories)
+            print('Enter any save file name.')
+            print('If you enter empty value, the file name set current date time.')
+            file_name = str(self.inputer.gets())
+            if not file_name:
+                now = datetime.datetime.now()
+                file_name = "data_{0:%Y%m%d_%H%M%S}.txt".format(now)
+            fizz_buzz.write(self.histories, file_name)
         elif selector == '4':
-            fizz_buzz.read(self.histories)
+            print('Enter any load file name.')
+            print('If you enter empty value, the file name set the newest file.')
+            files = [path.split('/')[-1] for path in  glob('data/*')]
+            print(files)
+            file_name = str(self.inputer.gets())
+            if not file_name:
+                pattern = r"data_[0-9]{8}_[0-9]{6}"
+                r = re.compile(pattern)
+                search_files = [file for file in files if r.match(file)]
+                if search_files:
+                    file_name = search_files[-1]
+            if file_name:
+                fizz_buzz.read(self.histories, self.printer, file_name)
+            else:
+                self.printer.error('Not found the newest data file.')
         else:
             self.printer.error('"%s" is not valid.' % selector)
